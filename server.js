5b361d3f1d038b0f8aad2248d9185858a060d236
Atul <atuls90901@gmail.com>
2026-05-20 13:20:45 +0530

    fix route file name case issue

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import morgan from "morgan";
 import cors from "cors";
 import path from "path";
 
-import routes from "./routes/userRoutes.js";
+import routes from "./routes/userroutes.js";
 
 dotenv.config();
 
